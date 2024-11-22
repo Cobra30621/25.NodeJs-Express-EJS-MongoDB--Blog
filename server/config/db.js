c895faced087d7e7696272c1fece3a8a673ed168
Cobra 陳俊宇 <cobra20621@gmail.com>
2024-11-22 23:13:49 +0800

    try fix bug

--- server/config/db.js
+++ server/config/db.js
@@ -3,16 +3,7 @@ const connectDB = async () => {
   
   try {
     mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      tls: true
-    }).then(() => {
-      console.log('Connected to MongoDB Atlas');
-    }).catch(err => {
-      console.error('Error connecting to MongoDB Atlas:', err);
-    });
-
+    const conn = await mongoose.connect(process.env.MONGODB_URI)
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
