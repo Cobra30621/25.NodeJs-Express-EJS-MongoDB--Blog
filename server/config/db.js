d7a17b0446b8d3e2ce7b74d0838392934f1d0e0f
Cobra 陳俊宇 <cobra20621@gmail.com>
2024-11-22 22:46:58 +0800

    update something

--- server/config/db.js
+++ server/config/db.js
@@ -3,11 +3,8 @@ const connectDB = async () => {
   
   try {
     mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      ssl: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI
+    );
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
